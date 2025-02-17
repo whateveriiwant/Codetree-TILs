83ed825fa85e527a8e5bab8d40154ac95fa66473
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-17 22:32:45 +0900

    Title: 전화번호 바꾸기, Time: 169ms, Memory: 15MB, Status: Passed - CodeTree

--- 250217/전화번호 바꾸기/change-phone-number.js
+++ 250217/전화번호 바꾸기/change-phone-number.js
@@ -1,3 +1,3 @@
-const [a, x, y] = require("fs").readFileSync(0).toString().trim().split("-").map(String);
+const [a, x, y] = require("fs").readFileSync(0).toString().trim().split("-");
 
 console.log(`${a}-${y}-${x}`);
