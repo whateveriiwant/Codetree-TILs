8c8cd192df1b1ca210cfd6905a34a0b014ae2b1d
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-22 00:25:21 +0900

    Title: 등급 매기기, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250222/등급 매기기/ranking.js
@@ -0,0 +1,13 @@
+const n = Number(require("fs").readFileSync(0).toString());
+
+if (n >= 90) {
+    console.log("A");
+} else if (n >= 80) {
+    console.log("B");
+} else if (n >= 70) {
+    console.log("C");
+} else if (n >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
