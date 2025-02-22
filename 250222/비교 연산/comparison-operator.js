6cc2097c5a0d37715ede5243272a3498af30afb3
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-22 23:19:39 +0900

    Title: 비교 연산, Time: 233ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250222/비교 연산/comparison-operator.js
@@ -0,0 +1,8 @@
+const [a, b] = require("fs").readFileSync(0).toString().split(" ").map(Number);
+
+console.log(a >= b ? "1" : "0");
+console.log(a > b ? "1" : "0");
+console.log(b >= a ? "1" : "0");
+console.log(b > a ? "1" : "0");
+console.log(a === b ? "1" : "0");
+console.log(a !== b ? "1" : "0");
