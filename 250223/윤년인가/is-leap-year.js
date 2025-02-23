be0154ca3019ed4e743bdc9d39e45a8864168e8b
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-23 22:10:45 +0900

    Title: 윤년인가, Time: 203ms, Memory: 15MB, Status: Passed - CodeTree

--- 250223/윤년인가/is-leap-year.js
+++ 250223/윤년인가/is-leap-year.js
@@ -1,6 +1,6 @@
 const y = Number(require("fs").readFileSync(0).toString());
 
-if (y % 4 === 0 && y % 100 !== 0) {
+if (y % 4 === 0 && y % 100 !== 0 || y % 400 === 0) {
     console.log("true");
 } else if (y % 100 === 0 && y % 400 !== 0) {
     console.log("false");
