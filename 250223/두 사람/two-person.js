e3a569d72b96171e0191efc4e3c2dbb8aaf12732
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-23 21:40:18 +0900

    Title: 두 사람, Time: 158ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250223/두 사람/two-person.js
+++ 250223/두 사람/two-person.js
@@ -3,4 +3,4 @@ const input = require("fs").readFileSync(0).toString().trim().split("\n");
 const a = input[0].split(" ");
 const b = input[1].split(" ");
 
-if ((a[0] >= 19 && a[1] === "M") || (b[0] >= 19 && b[1] === "W")) console.log(1); else console.log(0);
+if ((Number(a[0]) >= 19 && a[1] === "M") || (Number(b[0]) >= 19 && b[1] === "W")) console.log(1); else console.log(0);
