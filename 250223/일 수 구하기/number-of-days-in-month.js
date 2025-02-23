d579972c45314ee00c24ca0b386df2396a560db5
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-02-23 22:16:05 +0900

    Title: 일 수 구하기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250223/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,22 @@
+const n = Number(require("fs").readFileSync(0).toString());
+
+if (n === 4 || n === 6 || n === 9 || n === 11) {
+    console.log(30);
+} else if (n === 2) {
+    console.log(28);
+} else {
+    console.log(31);
+}
+
+1 - 31
+2 - 28
+3 - 31
+4 - 30
+5 - 31
+6 - 30
+7 - 31
+8 - 31
+9 - 30
+10 - 31
+11 - 30
+12 - 31
