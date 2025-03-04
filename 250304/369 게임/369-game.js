ccf4ae36695e0cda173c7b924c64d36bf3c015c5
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-04 21:55:25 +0900

    Title: 369 게임, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 250304/369 게임/369-game.js
+++ 250304/369 게임/369-game.js
@@ -2,21 +2,16 @@ const n = Number(require("fs").readFileSync(0).toString());
 let result = "";
 
 for (let i = 1; i < n+1; i++) {
-    let num = i;
-    let clap = false;
-
-    while (num > 0) {
-        let digit = num % 10;
-        if (digit % 3 === 0 || digit % 6 === 0 || digit % 9 === 0) {
-            clap = true;
-            break;
-        }
-        num = Math.floor(num / 10);
+    if (i % 3 === 0) {
+        result += "0 ";
     }
-
-    if (i % 3 === 0 || clap) {
-        result += 0 + " ";
-    } else {
+    else if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+        result += "0 ";
+    }
+    else if (parseInt(i / 10) === 3 || parseInt(i / 10) === 6 || parseInt(i / 10) === 9) {
+        result += "0 ";
+    }
+    else {
         result += i + " ";
     }
 }
