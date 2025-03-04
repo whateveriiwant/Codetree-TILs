03ad56877facc8fbd05b72ddb8a8c3c0f584fdf0
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-04 20:43:01 +0900

    Title: 높은 수에서 낮은 수까지, Time: 177ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250304/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,16 @@
+let [a, b] = require("fs").readFileSync(0).toString().split(" ").map(Number);
+let result = "";
+
+if (a > b) {
+    for (let i = a; i > b-1; i--) {
+        result += i + " ";
+    }
+}
+
+if (a < b) {
+    for (let i = b; i > a-1; i--) {
+        result += i + " ";
+    }
+}
+
+console.log(result);
