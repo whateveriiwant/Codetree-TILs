cceaab146e5532bcb245e5a8d2547b1ac40e6da9
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-05 20:37:48 +0900

    Title: 배수의 개수, Time: 178ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250305/배수의 개수/number-of-multipliers.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+let cnt3 = 0;
+let cnt5 = 0;
+
+for (let i = 0; i < 10; i++) {
+    if (Number(input[i] % 3 !== 0)) cnt3++;
+    if (Number(input[i] % 5 !== 0)) cnt5++;
+}
+
+console.log(cnt3, cnt5);
