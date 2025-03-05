3bd3134cd6041214cf1035204e5caf6aa69d53fa
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-05 21:54:16 +0900

    Title: 청소당번 정하기, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 250305/청소당번 정하기/cleaning-numbering.js
+++ 250305/청소당번 정하기/cleaning-numbering.js
@@ -4,32 +4,10 @@ let classroom = 0;
 let hallway = 0;
 let toilet = 0;
 
-for (let i = 0; i < n; i++) {
-    if (i > 0) {
-        if (i % 2 === 0) {
-            if (i % 3 === 0 && i % 12 !== 0) {
-                hallway++;
-            }
-            else if (i % 12 === 0) {
-                toilet++;
-            }
-            else {
-                classroom++;
-            }
-        }
-        else if (i % 3 === 0) {
-            if (i % 12 === 0) {
-                toilet++;
-            }
-            else {
-                hallway++;
-            }
-            
-        }
-        // else if (i % 12 === 0) {
-        //     toilet++;
-        // }
-    }
+for (let i = 1; i <= n; i++) {
+    if (i % 12 === 0) toilet++;
+    else if (i % 3 === 0) hallway++;
+    else if (i % 2 === 0) classroom++;
 }
 
 console.log(classroom, hallway, toilet);
