01141a9a58a7f504c6091ff71c70af916d24aeaf
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-05 20:49:07 +0900

    Title: 배수의 개수, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250305/배수의 개수/number-of-multipliers.js
+++ 250305/배수의 개수/number-of-multipliers.js
@@ -3,8 +3,8 @@ let cnt3 = 0;
 let cnt5 = 0;
 
 for (let i = 0; i < 10; i++) {
-    if (Number(input[i] % 3 !== 0)) cnt3++;
-    if (Number(input[i] % 5 !== 0)) cnt5++;
+    if (Number(input[i] % 3 === 0)) cnt3++;
+    if (Number(input[i] % 5 === 0)) cnt5++;
 }
 
 console.log(cnt3, cnt5);
