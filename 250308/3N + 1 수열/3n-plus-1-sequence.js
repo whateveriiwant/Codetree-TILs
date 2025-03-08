7c2109c4dabb67ede3c917091954305df82664cb
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-08 22:56:51 +0900

    Title: 3N + 1 수열, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

--- 250308/3N + 1 수열/3n-plus-1-sequence.js
+++ 250308/3N + 1 수열/3n-plus-1-sequence.js
@@ -2,13 +2,12 @@ let n = Number(require("fs").readFileSync(0).toString());
 let cnt = 0;
 
 while (true) {
+    if (n === 1) break;
     cnt++;
     if (n % 2 === 0) n /= 2;
     else {
         n = n * 3 + 1;
     }
-
-    if (n === 1) break;
 }
 
 console.log(cnt);
