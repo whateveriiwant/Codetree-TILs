afac732829a405cf6e77bb459573e8400e1cb140
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-08 23:15:17 +0900

    Title: ab 사이에 있는 c, Time: 161ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250308/ab 사이에 있는 c/c-between-a-and-b.js
@@ -0,0 +1,8 @@
+const [a, b, c] = require("fs").readFileSync(0).toString().trim().split(" ").map(Number);
+let status = false;
+
+for (let i = a; i <= b; i++) {
+    if (i % c === 0) status = true;
+}
+
+status ? console.log("YES") : console.log("NO");
