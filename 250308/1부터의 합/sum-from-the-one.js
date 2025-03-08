0640d8a5b7562c3d8ccbe279050024880615e848
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-08 20:18:39 +0900

    Title: 1부터의 합, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250308/1부터의 합/sum-from-the-one.js
+++ 250308/1부터의 합/sum-from-the-one.js
@@ -1,10 +1,9 @@
 const n = Number(require("fs").readFileSync(0).toString());
 let i = 1;
-let result = 1;
+let result = 0;
 
 while (true) {
     result += i;
-    
     if (result >= n) break;   
     i++;
 }
