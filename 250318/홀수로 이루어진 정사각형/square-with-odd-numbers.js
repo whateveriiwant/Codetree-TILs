ef75c13a729ecded8f3c810ddd03d84279ac0d96
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-18 22:10:42 +0900

    Title: 홀수로 이루어진 정사각형, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 250318/홀수로 이루어진 정사각형/square-with-odd-numbers.js
+++ 250318/홀수로 이루어진 정사각형/square-with-odd-numbers.js
@@ -2,8 +2,8 @@ const n = Number(require("fs").readFileSync(0).toString());
 let result = "";
 
 for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j += 2) {
-        result += `${i * 2 + j * 2 + 11}`;
+    for (let j = 0; j < n; j++) {
+        result += `${i * 2 + j * 2 + 11} `;
     }
     result += "\n";
 }
