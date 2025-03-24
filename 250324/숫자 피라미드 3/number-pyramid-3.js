583ff59161658ebc59d8d1e92502d32e4a27ea74
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-03-24 21:31:32 +0900

    Title: 숫자 피라미드 3, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250324/숫자 피라미드 3/number-pyramid-3.js
@@ -0,0 +1,11 @@
+const n = Number(require("fs").readFileSync(0).toString());
+let result = "";
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        result += i * j + " ";
+    }
+    result += "\n";
+}
+
+console.log(result);
