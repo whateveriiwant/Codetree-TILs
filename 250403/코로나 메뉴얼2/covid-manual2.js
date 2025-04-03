67483081075938b8d3288925ab100f0af0d29e71
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-04-03 16:55:39 +0900

    Title: 코로나 메뉴얼2, Time: 175ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250403/코로나 메뉴얼2/covid-manual2.js
+++ 250403/코로나 메뉴얼2/covid-manual2.js
@@ -1,23 +1,16 @@
-const input = require('fs').readFileSync(0).toString().trim().split("\n");
+let [a, b] = require("fs").readFileSync(0).toString().trim().split(" ").map(Number);
 
-let arr = Array(4); // 진료소 인원 배열
-arr.fill(0);
+let cnt = Array(b);
+cnt.fill(0);
 
-for (let i = 0; i < 3; i++) {
-    const patient = input[i].trim().split(" "); // [증상여부, 체온]
-    
-    if (patient[0] === 'Y' && patient[1] >= 37) arr[0]++;
-    else if (patient[0] === 'N' && patient[1] >= 37) arr[1]++;
-    else if (patient[0] === 'Y' && patient[1] < 37) arr[2]++;
-    else arr[3]++;
+while (a > 1) {
+    cnt[a % b]++;
+    a = parseInt(a / b);
 }
 
-let result = "";
-
-for (let i = 0; i < 4; i++) {
-    result += arr[i] + ' ';
+let sum = 0;
+for (let i = 0; i < b; i++) {
+    sum += cnt[i] * cnt[i];
 }
 
-if (arr[0] >= 2) result += 'E';
-
-console.log(result);
+console.log(sum);
