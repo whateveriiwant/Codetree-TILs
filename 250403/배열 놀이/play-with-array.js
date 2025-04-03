82b8682550132c5ac4c744a09b9d5416e2c2a2b9
Seung Jun Jeong <seungjun.jeong@studentambassadors.com>
2025-04-03 17:20:18 +0900

    Title: 배열 놀이, Time: 228ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250403/배열 놀이/play-with-array.js
+++ 250403/배열 놀이/play-with-array.js
@@ -7,18 +7,20 @@ const numbers = input[1].split(" ").map(Number); // 원소 배열
 for (let i = 2; i < q + 2; i++) { // 질의 입력
     const question = input[i].split(" ").map(Number);
 
-    if (question[0] === 1) {
-        console.log(numbers[question[1]-1]);
+    if (question[0] === 1) { // 1 a
+        const a = question[1];
+        console.log(numbers[a - 1]);
     }
-    else if (question[0] === 2) {
+    else if (question[0] === 2) { // 2 b
+        const b = question[1];
         for (let j = 0; j < numbers.length; j++) {
-            if (numbers[j] === question[1]) {
+            if (numbers[j] === b) {
                 console.log(j+1);
                 break;
-            }
+            } else console.log(0);
         }
     }
-    else if (question[0] === 3) {
+    else if (question[0] === 3) { // 3 c
         let result = "";
         for (let j = question[1]; j <= question[2]; j++) {
             result += numbers[j-1] + " ";
