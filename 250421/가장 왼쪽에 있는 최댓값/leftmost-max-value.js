dd804e94c7023d9f50b1ef7a3b3b1d33f8c00f1d
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-21 10:44:28 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250421/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -0,0 +1,25 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+let result = "";
+
+let max1 = arr[0];
+let index1 = 0;
+
+for (let i = 0; i < n; i++) {
+    if (max1 < arr[i]) {
+        index1 = i;
+        max1 = arr[i];
+    }
+}
+
+result += (index1 + 1) + " ";
+
+for (let i = index1 - 1; i >= 0; i--) { // 최댓값 왼쪽 인덱스부터 시작
+    let tmp = arr[i];
+    if (tmp === arr[i-1]) continue;
+    result += (i + 1) + " ";
+}
+
+console.log(result);
