567127f8711e904c87c73c4c001756a1b0424b30
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-21 10:57:41 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 207ms, Memory: 15MB, Status: Passed - Codetree

--- 250421/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
+++ 250421/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -4,26 +4,20 @@ const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
 let result = "";
 
-let max1 = arr[0];
-let index1 = 0;
+let prevMaxIdx = n;
 
-for (let i = 0; i < n; i++) {
-    if (max1 < arr[i]) {
-        index1 = i;
-        max1 = arr[i];
+while (true) {
+    let max = 0;
+
+    for (let i = 1; i < prevMaxIdx; i++) {
+        if (arr[i] > arr[max]) max = i;
     }
-}
 
-result += (index1 + 1) + " ";
-let cnt = 0;
+    result += `${max + 1} `;
 
-for (let i = index1 - 1; i >= 0; i--) { // 최댓값 왼쪽 인덱스부터 시작
-    let tmp = arr[i];
-    if (tmp === arr[i-1]) {
-        cnt++;
-        continue;
-    }
-    if (cnt >= 1) result += (i + 1) + " ";
+    if (max === 0) break;
+
+    prevMaxIdx = max;
 }
 
 console.log(result);
