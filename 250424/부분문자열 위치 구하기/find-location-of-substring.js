87d88cda6b16bb35c1147e7b48f7d75b44fd65eb
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-24 11:08:30 +0900

    Title: 부분문자열 위치 구하기, Time: 183ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250424/부분문자열 위치 구하기/find-location-of-substring.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const inputStr = input[0];
+const findStr = input[1];
+let idx = -1;
+
+for (let i = 0; i < inputStr.length - 1; i++) {
+    if (inputStr[i] + inputStr[i+1] === findStr) {
+        idx = i;
+        break;
+    }
+}
+
+console.log(idx);
