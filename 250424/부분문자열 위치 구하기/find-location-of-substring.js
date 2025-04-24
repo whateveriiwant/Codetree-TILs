d06a9815555367c6625dc8f03da83fe67d889aa6
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-24 11:22:17 +0900

    Title: 부분문자열 위치 구하기, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250424/부분문자열 위치 구하기/find-location-of-substring.js
+++ 250424/부분문자열 위치 구하기/find-location-of-substring.js
@@ -4,14 +4,7 @@ const inputStr = input[0];
 const findStr = input[1];
 let idx = -1;
 
-for (let i = 0; i < inputStr.length - 1; i++) {
-    if (inputStr[i] + inputStr[i+1] === findStr) {
-        idx = i;
-        break;
-    }
-}
-
-if (findStr.length === 1) idx = inputStr.indexOf(findStr);
+idx = inputStr.indexOf(findStr);
 
 if (inputStr === findStr) console.log(0);
 else console.log(idx);
