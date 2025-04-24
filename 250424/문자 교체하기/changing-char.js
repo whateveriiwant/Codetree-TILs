82c9c18dbbca1849c01832ac2afbd24983360605
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-24 13:20:50 +0900

    Title: 문자 교체하기, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

--- 250424/문자 교체하기/changing-char.js
+++ 250424/문자 교체하기/changing-char.js
@@ -1,7 +1,7 @@
 const [a, b] = require("fs").readFileSync(0).toString().trim().split(" ");
 
 let arr1 = a.split('');
-let arr2 = a.split('');
+let arr2 = b.split('');
 
 arr2[0] = arr1[0];
 arr2[1] = arr1[1];
