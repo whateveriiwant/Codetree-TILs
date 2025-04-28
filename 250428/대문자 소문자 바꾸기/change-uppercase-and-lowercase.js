b827777fc2b952b4b11fd81ec32558c65513711a
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-28 16:25:49 +0900

    Title: 대문자 소문자 바꾸기, Time: 177ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250428/대문자 소문자 바꾸기/change-uppercase-and-lowercase.js
@@ -0,0 +1,10 @@
+let str = require("fs").readFileSync(0).toString().trim().split();
+
+let result = "";
+
+for (let i of str) {
+    if (i.charCodeAt(0) >= 'a'.charCodeAt(0) && i.charCodeAt(0) <= 'z'.charCodeAt(0)) result += i.toUpperCase();
+    else if (i.charCodeAt(0) >= 'A'.charCodeAt(0) && i.charCodeAt(0) <= 'Z'.charCodeAt(0)) result += i.toLowerCase();
+}
+
+console.log(result);
