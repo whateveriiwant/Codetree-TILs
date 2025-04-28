8eb1ca14a33973540e7345e981594567588798cc
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-28 15:08:43 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 248ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250428/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+let [str, q] = input[0].split(" ");
+
+for (let i = 1; i <= q; i++) {
+    let request = Number(input[i]);
+
+    if (request === 1) {
+        str = str.slice(1) + str.slice(0, 1);
+    } else if (request === 2) {
+        str = str.slice(-1) + str.slice(0, -1);
+    } else {
+        let arr = str.split("");
+        arr.reverse();
+        str = arr.join("");
+    }
+    console.log(str);
+}
