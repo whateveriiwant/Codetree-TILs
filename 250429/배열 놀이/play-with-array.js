e257dea72bb739398de4ec197be72a891c25ce1b
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-04-29 16:21:12 +0900

    Title: 배열 놀이, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

--- 250429/배열 놀이/play-with-array.js
+++ 250429/배열 놀이/play-with-array.js
@@ -1,35 +1,24 @@
-const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const input = require('fs').readFileSync(0).toString().trim().split('\n');
+const [n, q] = input[0].split(' ').map(Number);
+const elements = input[1].split(' ').map(Number);
 
-const [n, q] = input[0].split(" ").map(Number);
+for (let i = 2; i < 2 + q; i++) {
+    const query = input[i].split(' ').map(Number);
+    const type = query[0];
 
-const elements = input[1].split(" ").map(Number);
-
-for (let i = 2; i <= q+1; i++) {
-    const question = input[i].split(" ").map(Number);
-    const questionType = question[0];
-
-    if (questionType === 1) {
-        const a = question[1];
-        console.log(elements[a-1]);
-    } else if (questionType === 2) {
-        const b = question[1];
-        let index = 0;
-
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i] === b) {
-                index = i;
-                break;
-            } 
-        }
-        index !== 0 ? console.log(index+1) : console.log(index);
-    } else {
-        const s = question[1];
-        const e = question[2];
-        let result = "";
-
-        for (let i = s-1; i <= e-1; i++) {
-            result += `${elements[i]} `;
-        }
-        console.log(result);
+    if (type === 1) {
+        // 1 a: a번째 원소 출력 (1-based)
+        const a = query[1];
+        console.log(elements[a - 1]);
+    } else if (type === 2) {
+        // 2 b: 값이 b인 첫 번째 원소의 인덱스(1-based) 출력, 없으면 0
+        const b = query[1];
+        const idx = elements.indexOf(b);
+        console.log(idx === -1 ? 0 : idx + 1);
+    } else if (type === 3) {
+        // 3 s e: s번째~e번째 원소 출력 (1-based, 공백 구분)
+        const s = query[1];
+        const e = query[2];
+        console.log(elements.slice(s - 1, e).join(' '));
     }
 }
