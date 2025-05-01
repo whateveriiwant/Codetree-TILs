6fd599bf5fb681bc8f6cee4db627702fd141a159
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-01 14:33:12 +0900

    Title: 2021년 날짜의 유무, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

--- 250501/2021년 날짜의 유무/with-or-without-2021.js
+++ 250501/2021년 날짜의 유무/with-or-without-2021.js
@@ -2,18 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 let [m, d] = input[0].split(" ").map(Number);
 
-const func = (m, d) => {
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-        if (d <= 31 && d >= 1) return true;
-        else return false;
-    } else if (m == 4 || m == 6 || m == 9 || m == 11) {
-        if (d <= 30 && d >= 1) return true;
-        else return false;
-    } else if (m == 2) {
-        if (d <= 28 && d >= 1) return true;
-        else return false;
+const lastDayNumber = (m) => {
+    if (m === 2) return 28;
+    if (m === 4 || m === 6 || m === 9 || m === 11) return 30;
+    return 31;
+}
+
+const judgeDay = (m, d) => {
+    if (m <= 12 && d <= lastDayNumber(m)) {
+        return true;
     }
     return false;
-}; 
+};
 
-console.log(func(m, d) ? "Yes" : "No");
+console.log(judgeDay(m, d) ? "Yes" : "No");
