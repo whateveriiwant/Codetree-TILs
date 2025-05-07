7aca567cafc7a84fc6ca7c365c92bac071c1863e
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-07 21:42:29 +0900

    Title: 재귀함수의 꽃, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

--- 250507/재귀함수의 꽃/the-flower-of-the-recursive-function.js
+++ 250507/재귀함수의 꽃/the-flower-of-the-recursive-function.js
@@ -3,15 +3,11 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let n = Number(input[0]);
 
 const print = (n) => {
+    if (n === 0) return;
+
     process.stdout.write(n + " ");
-
-    if (n > 1) {
-        print(n - 1);
-    } 
-
-    if (n >= 1) {
-        process.stdout.write(n + " ");
-    }
+    print(n-1);
+    process.stdout.write(n + " ");
 };
 
 print(n);
