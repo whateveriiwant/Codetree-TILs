a13f1284add6988f100d41577614a33b4157cc0f
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-13 13:41:28 +0900

    Title: Top K 숫자 구하기, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250513/Top K 숫자 구하기/kth-number.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input[1].trim().split(' ').map(Number);
+
+nums.sort((a, b) => a - b);
+
+console.log(nums[k-1]);
