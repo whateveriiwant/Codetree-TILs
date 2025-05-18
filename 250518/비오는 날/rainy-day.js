3cf21452a52d1087380c51ee7b1456a5f7ce8df7
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-18 16:46:40 +0900

    Title: 비오는 날, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250518/비오는 날/rainy-day.js
@@ -0,0 +1,22 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = [];
+
+class Info {
+    constructor(date, weekday, fc) {
+        this.date = date;
+        this.weekday = weekday;
+        this.fc = fc;
+    }
+};
+
+for (let i = 1; i <= n; i++) {
+    const data = new Info(input[i].split(" ")[0], input[i].split(" ")[1], input[i].split(" ")[2]);
+
+    if (data.fc === "Rain") arr.push(data);
+}
+
+arr.sort();
+
+console.log(arr[0].date, arr[0].weekday, arr[0].fc);
