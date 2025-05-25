e523edfe9ef7a40d4f5600d12d745f942b59a112
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-25 15:30:27 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 265ms, Memory: 61MB, Status: Runtime Error - Codetree

---
+++ 250525/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const rect1 = input[0].split(' ').map(Number);
+const rect2 = input[1].split(' ').map(Number);
+
+const arr = Array(2001).fill(0).map(() => Array(2001).fill(0));
+
+for (let i = 0; i < 2; i++) {
+    const [x1, y1, x2, y2] = i === 0 ? rect1.map(Number) : rect2.map(Number);
+
+    for (let j = x1; j < x2; j++) {
+        for (let k = y1; k < y2; k++) {
+            if (arr[j][k] === 1) arr[j][k] = 3;
+            else arr[j][k] = i === 0 ? 1 : 2;
+        }
+    }
+}
+
+let sum = 0;
+for (let i of arr) {
+    for (let j of i) {
+        if (j === 1 || j === 3) sum++;
+    }
+}
+
+console.log(sum);
