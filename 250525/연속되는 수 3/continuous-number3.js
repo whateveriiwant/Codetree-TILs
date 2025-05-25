e8901585b2ad6e982d58a7b9280e96c75290e4eb
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-25 17:15:08 +0900

    Title: 연속되는 수 3, Time: 170ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250525/연속되는 수 3/continuous-number3.js
+++ 250525/연속되는 수 3/continuous-number3.js
@@ -6,7 +6,7 @@ const arr = input.slice(1, n + 1).map(Number);
 let cnt = 1;
 let maxCnt = 1;
 
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n - 1; i++) {
     if ((arr[i] > 0 && arr[i + 1] > 0) || (arr[i] < 0 && arr[i + 1] < 0)) cnt++;
     else {
         maxCnt = Math.max(maxCnt, cnt);
