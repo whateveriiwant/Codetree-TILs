72246e86f056bf6d08b4e39d45473e14cb8adeb0
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-25 19:41:09 +0900

    Title: 연속되는 수 4, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250525/연속되는 수 4/continuous-number4.js
+++ 250525/연속되는 수 4/continuous-number4.js
@@ -5,7 +5,7 @@ const numbers = input.slice(1, n + 1).map(Number);
 let cnt = 1;
 let maxCnt = 1;
 
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n - 1; i++) {
     if (numbers[i] < numbers[i + 1]) cnt++;
     else {
         maxCnt = Math.max(maxCnt, cnt);
@@ -13,4 +13,4 @@ for (let i = 0; i < n; i++) {
     }
 }
 
-console.log(maxCnt);
+console.log(Math.max(maxCnt, cnt));
