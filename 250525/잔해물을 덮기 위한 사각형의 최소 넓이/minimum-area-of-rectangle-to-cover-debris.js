651638291ab83ad0f20d8f228003a5b9d54278db
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-25 15:31:29 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 311ms, Memory: 62MB, Status: Wrong Answer - Codetree

--- 250525/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 250525/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -5,14 +5,15 @@ const rect1 = input[0].split(' ').map(Number);
 const rect2 = input[1].split(' ').map(Number);
 
 const arr = Array(2001).fill(0).map(() => Array(2001).fill(0));
+const OFFSET = 1000;
 
 for (let i = 0; i < 2; i++) {
     const [x1, y1, x2, y2] = i === 0 ? rect1.map(Number) : rect2.map(Number);
 
     for (let j = x1; j < x2; j++) {
         for (let k = y1; k < y2; k++) {
-            if (arr[j][k] === 1) arr[j][k] = 3;
-            else arr[j][k] = i === 0 ? 1 : 2;
+            if (arr[j + OFFSET][k + OFFSET] === 1) arr[j + OFFSET][k + OFFSET] = 3;
+            else arr[j + OFFSET][k + OFFSET] = i === 0 ? 1 : 2;
         }
     }
 }
