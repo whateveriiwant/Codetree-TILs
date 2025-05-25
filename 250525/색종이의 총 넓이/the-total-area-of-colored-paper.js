742c54449fee3978d99d0dc5661ff8c535384e4f
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-25 15:19:53 +0900

    Title: 색종이의 총 넓이, Time: 162ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250525/색종이의 총 넓이/the-total-area-of-colored-paper.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const rects = input.slice(1, n + 1).map(line => line.split(" ").map(Number));
+
+const arr = Array(201).fill(0).map(() => Array(201).fill(0));
+const OFFSET = 100;
+
+for (let i = 0; i < n; i++) {
+    const [x1, y1] = rects[i].split(" ").map(Number);
+    const [x2, y2] = [x1 + 8, y1 + 8];
+
+    for (let j = x1; j < x2; j++) {
+        for (let k = y1; k < y2; k++) {
+            arr[j + OFFSET][k + OFFSET] = 1;
+        }
+    }
+}
+
+let sum = 0;
+for (let i of arr) {
+    for (let j of i) {
+        if (j === 1) sum++;
+    }
+}
+
+console.log(sum);
