6282bc4d51622b964613a52baf3b10c0d7008472
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-27 16:34:16 +0900

    Title: 좌우로 움직이는 로봇, Time: 456ms, Memory: 72MB, Status: Passed - Codetree

--- 250527/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
+++ 250527/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -15,13 +15,8 @@ for (let i of moveA) {
     const [t, d] = i.split(' ');
 
     for (let j = 0; j < t; j++) {
-        if (d == 'L') {
-            posA--;
-            arrA.push(posA);
-        } else {
-            posA++;
-            arrA.push(posA);
-        }
+        d == 'L' ? posA-- : posA++;
+        arrA.push(posA);
     }
 }
 
@@ -29,21 +24,27 @@ for (let i of moveB) {
     const [t, d] = i.split(' ');
 
     for (let j = 0; j < t; j++) {
-        if (d == 'L') {
-            posB--;
-            arrB.push(posB);
-        } else {
-            posB++;
-            arrB.push(posB);
-        }
+        d == 'L' ? posB-- : posB++;
+        arrB.push(posB);
     }
 }
 
 const len = Math.max(arrA.length, arrB.length);
 let cnt = 0;
+
+// A 로봇이 먼저 끝났다면, 마지막 위치에 계속 머물러 있음
+while (arrA.length < len) {
+    arrA.push(arrA[arrA.length - 1]);
+}
+
+// B 로봇이 먼저 끝났다면, 마지막 위치에 계속 머물러 있음
+while (arrB.length < len) {
+    arrB.push(arrB[arrB.length - 1]);
+}
+
 for (let i = 1; i < len; i++) {
-    if (arrA[i] != arrB[i]) {
-        if (arrA[i + 1] == arrB[i + 1]) cnt++;
+    if ((arrA[i - 1] !== arrB[i - 1]) && (arrA[i] === arrB[i])) {
+        cnt++;
     }
 }
 
