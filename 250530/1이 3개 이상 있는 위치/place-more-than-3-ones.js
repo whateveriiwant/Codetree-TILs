f5ea99420d184df2fb59259cc34e9a75e82ac101
Seung Jun Jeong <jsj9620@hufs.ac.kr>
2025-05-30 12:30:33 +0900

    Title: 1이 3개 이상 있는 위치, Time: 172ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250530/1이 3개 이상 있는 위치/place-more-than-3-ones.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1).map(line => line.split(' ').map(Number));
+
+const dx = [0, 1, 0, -1]; // 북 동 남 서
+const dy = [1, 0, -1, 0];
+
+let result = 0;
+
+const inRange = (x, y) => {
+    return x >= 0 && x < n && y >= 0 && y < n;
+};
+
+for (let i = 0; i < n; i++) {
+    let cnt = 0;
+    for (let j = 0; j < n; j++) {
+        for (let dirNum = 0; dirNum < 4; dirNum++) {
+            const nx = i + dx[dirNum];
+            const ny = j + dy[dirNum];
+            if (inRange(nx, ny) && grid[nx][ny] === 1) cnt++;
+        }
+        if (cnt >= 3) result++;
+    }
+}
+
+console.log(result);
