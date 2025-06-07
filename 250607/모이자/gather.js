f741ed79098d339b8bca0296a6de469b6df07c6b
Seungjun Jeong <jsj9620@hufs.ac.kr>
2025-06-07 15:09:39 +0900

    Title: 모이자, Time: 172ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250607/모이자/gather.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number); // 각 집에 사는 사람 수
+
+let min = 100;
+for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = 0; j < n; j++) {
+        sum += arr[j] * Math.abs(j - i);
+    }
+    if (sum < min) min = sum;
+}
+
+console.log(min);
