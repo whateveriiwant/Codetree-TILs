e19b2b2d74d46a96bff082c47fd92f8b794e7f62
Seungjun Jeong <jsj9620@hufs.ac.kr>
2025-06-07 15:16:11 +0900

    Title: 모이자, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250607/모이자/gather.js
+++ 250607/모이자/gather.js
@@ -4,13 +4,13 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number); // 각 집에 사는 사람 수
 
-let min = Math.MAX_SAFE_INTEGER;
+let min = 0;
 for (let i = 0; i < n; i++) {
     let sum = 0;
     for (let j = 0; j < n; j++) {
         sum += arr[j] * Math.abs(j - i);
     }
-    if (sum < min) min = sum;
+    if (sum < min || min === 0) min = sum;
 }
 
 console.log(min);
