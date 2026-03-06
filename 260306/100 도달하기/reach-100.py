8efcaa7b719a2b95f34f2105e1a18a0cacfbd163
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-03-06 11:45:19 +0900

    Title: 100 도달하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260306/100 도달하기/reach-100.py
+++ 260306/100 도달하기/reach-100.py
@@ -4,8 +4,8 @@ arr = [1, n]
 
 while True:
     new_val = arr[-1] + arr[-2]
-    if new_val > 100: break
     arr.append(new_val)
+    if new_val > 100: break
 
 for i in arr:
     print(i, end = ' ')
