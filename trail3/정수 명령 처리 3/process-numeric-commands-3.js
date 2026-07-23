6b86cf88835af9f9b00ad2dd58db1cb77d83b6ca
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-07-23 09:59:57 +0900

    [Passed] 정수 명령 처리 3 (80ms, 11MB)

---
+++ trail3/정수 명령 처리 3/process-numeric-commands-3.js
@@ -0,0 +1,123 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class Deque {
+    constructor() {
+        this.count = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    pushFront(A) {
+        let x = new Node(A);
+
+        if (this.count === 0) {
+            this.head = x;
+            this.tail = x;
+        } else {
+            this.head.prev = x;
+            x.next = this.head;
+            this.head = x;
+        }
+        this.count += 1;
+    }
+
+    pushBack(A) {
+        let x = new Node(A);
+
+        if (this.count === 0) {
+            this.head = x;
+            this.tail = x;
+        } else {
+            this.tail.next = x;
+            x.prev = this.tail;
+            this.tail = x;
+        }
+        this.count += 1;
+    }
+
+    popFront() {
+        let x = this.head;
+        if (this.count === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = x.next;
+            this.head.prev = null;
+        }
+        this.count -= 1;
+        return x.value;
+    }
+
+    popBack() {
+        let x = this.tail;
+        if (this.count === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = x.prev;
+            this.tail.next = null;
+        }
+        this.count -= 1;
+        return x.value;
+    }
+
+    size() {
+        return this.count;
+    }
+
+    empty() {
+        return this.count === 0 ? 1 : 0;
+    }
+
+    front() {
+        return this.head.value;
+    }
+
+    back() {
+        return this.tail.value;
+    }
+}
+
+const input = require('fs').readFileSync(0).toString().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1);
+
+const deque = new Deque();
+
+for (let i = 0; i < n; i += 1) {
+    const [cmd, num] = commands[i].split(' ');
+
+    switch (cmd) {
+        case 'push_back':
+            deque.pushBack(num);
+            break;
+        case 'push_front':
+            deque.pushFront(num);
+            break;
+        case 'pop_front':
+            console.log(deque.popFront());
+            break;
+        case 'pop_back':
+            console.log(deque.popBack());
+            break;
+        case 'size':
+            console.log(deque.size());
+            break;
+        case 'empty':
+            console.log(deque.empty());
+            break;
+        case 'front':
+            console.log(deque.front());
+            break;
+        case 'back':
+            console.log(deque.back());
+            break;
+    }
+}
